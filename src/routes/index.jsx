507c9b6225c0de7dbf22c95ba5e0c7ddc0bb8f67
Seonghoon Jeong <hooni09061201@gmail.com>
2025-06-20 16:03:56 +0900

    create login page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import Splash from '../pages/Splash';
+import LoginPage from '../pages/LoginPage';
 
 const AppRoutes = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Splash />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   </Router>
 );
