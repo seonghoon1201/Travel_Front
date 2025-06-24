c4843b70946556958be2ede83e69c56143673966
Seonghoon Jeong <hooni09061201@gmail.com>
2025-06-24 13:49:34 +0900

    localStorage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,10 +1,11 @@
 export const setItem = (key, value) => {
+  if (value === undefined) return;
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getItem = (key) => {
+export const getItem = (key, defaultValue = null) => {
   const value = localStorage.getItem(key);
-  return value ? JSON.parse(value) : null;
+  return value ? JSON.parse(value) : defaultValue;
 };
 
 export const removeItem = (key) => {
