37107b99c81f8e2bbf56f5ee536a0935b0fb4dce
doheun9489 <doheun9489@gmail.com>
2025-06-24 11:10:51 +0900

    feature/Mypage 여행일기 UI 구현

--- src/components/mypage/MyDiarySection.jsx
+++ src/components/mypage/MyDiarySection.jsx
@@ -1,3 +1,18 @@
-const MyDiarySection = () => <div className="p-4">여행 일기 콘텐츠</div>;
+import React from 'react';
+import TravelDiary from '../../components/traveldiary/TravelDiary';
+
+const MyDiarySection = () => {
+  return (
+    <div className="p-4">
+      <TravelDiary
+        title="6월의 제주"
+        nickname="닉네임"
+        period="3박 4일"
+        tags={['제주', '6월', '여박여행']}
+        imageUrl="https://example.com/jeju.jpg"
+      />
+    </div>
+  );
+};
 
 export default MyDiarySection;
