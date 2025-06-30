cbde61fb9903e5affb6bcfc454ef6319f635ef44
doheun9489 <doheun9489@gmail.com>
2025-06-30 16:19:17 +0900

    feature/hotboard 컴포넌트 명 변경 (UI수정)

--- src/components/board/PlaceList.jsx
+++ src/components/board/PlaceList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HotSpotItem = ({
+const PlaceList = ({
   destination,
   category,
   location,
@@ -36,4 +36,4 @@ const HotSpotItem = ({
   );
 };
 
-export default HotSpotItem;
+export default PlaceList;
