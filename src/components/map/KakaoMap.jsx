4cb0cc1a3c6e34959ecb6c5835b9408802a840d5
doheun9489 <doheun9489@gmail.com>
2025-07-14 09:24:36 +0900

    로컬 변경 사항 커밋

--- src/components/map/KakaoMap.jsx
+++ src/components/map/KakaoMap.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect } from 'react';
 import { loadKakaoMapScript } from '../../utils/kakaoMapLoader';
 
-const KakaoMap = () => {
+const GKakaoMap = () => {
   useEffect(() => {
     loadKakaoMapScript(() => {
-        console.log('지도 콜백 시작');
-        // container: 지도가 들어갈 <div id="map">
-        const container = document.getElementById('map');
-        // 추후에 api 연동해서 위도 경도값 변수로 받아와서 해야할 듯 !
-        //LatLng: 위도 경도, level: 줌 레벨, Map: 지도를 생성해서 넣기
-        const options = {
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3,
-        };
-        const map = new window.kakao.maps.Map(container, options);
+      console.log('지도 콜백 시작');
+      // container: 지도가 들어갈 <div id="map">
+      const container = document.getElementById('map');
+      // 추후에 api 연동해서 위도 경도값 변수로 받아와서 해야할 듯 !
+      //LatLng: 위도 경도, level: 줌 레벨, Map: 지도를 생성해서 넣기
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(container, options);
 
-        new window.kakao.maps.Marker({
-            map,
-            position: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        });
-        });
+      new window.kakao.maps.Marker({
+        map,
+        position: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      });
+    });
   }, []);
 
   return <div id="map" style={{ width: '100%', height: '400px' }}></div>;
