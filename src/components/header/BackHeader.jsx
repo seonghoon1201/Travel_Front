fdccde1babb251181bac133ee03caffa545c2a20
doheun9489 <doheun9489@gmail.com>
2025-07-25 17:19:45 +0900

    style: backheader 왼쪽 여백 설정

--- src/components/header/BackHeader.jsx
+++ src/components/header/BackHeader.jsx
@@ -13,7 +13,7 @@ const BackHeader = ({
   return (
     <header className="flex items-center justify-center px-4 py-3 relative mb-6 mt-6">
       <button onClick={() => navigate(-1)}>
-        <ArrowLeft className="w-5 h-5 text-black absolute left-3 top-3" />
+        <ArrowLeft className="w-5 h-5 text-black absolute left-5 top-3" />
       </button>
       <h1 className="font-noonnu text-2xl absolute left-1/2 -translate-x-1/2 font-extrabold">
         {title}
