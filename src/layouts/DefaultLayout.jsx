bb2450cd1ee65fb50471d40b9c924b765d61e325
doheun9489 <doheun9489@gmail.com>
2025-07-25 12:43:28 +0900

    layout 여백 조정

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DefaultLayout = ({ children }) => {
   return (
-    <div className="bg-background min-h-screen flex flex-col w-full font-pretendard px-4 py-6">
+    <div className="bg-background min-h-screen flex flex-col w-full font-pretendard ">
       <div className="w-full mx-auto">{children}</div>
     </div>
   );
