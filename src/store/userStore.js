50b9d5e14d3d512e846dea0f32b278edd0de9e36
doheun9489 <doheun9489@gmail.com>
2025-07-25 17:39:59 +0900

    style: 여백 재설정

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -9,33 +9,44 @@ const initialState = {
   userRole: getItem('userRole', ''),
   userEmail: getItem('userEmail', ''),
   userName: getItem('userName', ''),
+  // Boolean 상태로 로그인 여부 저장
+  isLoggedIn: !!getItem('accessToken', ''),
 };
 
 const useUserStore = create((set) => ({
   ...initialState,
 
+  // 로그인 시 모든 데이터 저장 + 로그인 상태 true
   login: (userData) => {
-    set(userData);
+    set({ ...userData, isLoggedIn: true });
     Object.entries(userData).forEach(([key, value]) => {
       setItem(key, value);
     });
+    setItem('isLoggedIn', true);
   },
 
+  // 로그아웃 시 모든 데이터 초기화 + 로그인 상태 false
   logout: () => {
     const cleared = {};
     Object.keys(initialState).forEach((key) => {
       cleared[key] = '';
       removeItem(key);
     });
+    cleared.isLoggedIn = false;
+    removeItem('isLoggedIn');
     set(cleared);
   },
 
+  // 새로고침 시 저장된 값 복원
   initializeFromStorage: () => {
     const restored = {};
     Object.keys(initialState).forEach((key) => {
       restored[key] = getItem(key, '');
     });
-    set(restored);
+    set({
+      ...restored,
+      isLoggedIn: !!getItem('accessToken', ''),
+    });
   },
 
   setNickname: (nickname) => {
