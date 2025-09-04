3835a5fdf8ecd51e5d732bf82cd6c79505cdfa77
doheun9489 <doheun9489@gmail.com>
2025-09-04 17:23:49 +0900

    feat: 수정

---
+++ src/components/header/DiaryHeader.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
+
+const DiaryHeader = ({
+  title = '',
+  showRightButton = false,
+  rightButtonText = '',
+  onRightButtonClick = () => {},
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <header className="flex items-center justify-center px-4 py-3 relative mb-6 mt-6">
+      <button onClick={() => navigate('/board/travel/diary')}>
+        <ArrowLeft className="w-5 h-5 text-black absolute left-5 top-3" />
+      </button>
+      <h1 className="font-noonnu text-2xl absolute left-1/2 -translate-x-1/2 font-extrabold">
+        {title}
+      </h1>
+      <div className="w-5 h-5">
+        {showRightButton && (
+          <button
+            onClick={onRightButtonClick}
+            className="absolute right-4 text-sm text-blue-500"
+          >
+            {rightButtonText}
+          </button>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default DiaryHeader;
