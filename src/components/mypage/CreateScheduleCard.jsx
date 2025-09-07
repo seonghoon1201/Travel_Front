1f62f92b132a90585f5f1681312b26119b853e0c
doheun9489 <doheun9489@gmail.com>
2025-09-07 15:45:02 +0900

    fix: 여행일정 만들기 멘트 및 정사각형 유지

--- src/components/mypage/CreateScheduleCard.jsx
+++ src/components/mypage/CreateScheduleCard.jsx
@@ -14,7 +14,7 @@ const CreateScheduleCard = () => {
       onClick={handleClick}
       className=" p-4 mb-6 rounded-xl bg-[#DDF1FB] border border-blue-100 hover:shadow-md transition cursor-pointer flex items-center gap-4"
     >
-      <div className="w-10 h-10 rounded-full bg-white border border-blue-300 flex items-center justify-center text-xl font-bold shadow-sm">
+      <div className="w-10 aspect-square rounded-full bg-white border border-blue-300 flex items-center justify-center text-xl font-bold shadow-sm">
         <CalendarPlus />
       </div>
 
@@ -23,7 +23,7 @@ const CreateScheduleCard = () => {
           여행 일정 만들기
         </p>
         <p className="-mt-3 font-noonnu text-xs text-gray-500 ">
-          친구와 여행 계획부터 경비까지 함께 해보세요!
+          복잡한 계획은 이제 그만! 장바구니에 담기만 하면 자동으로 최적 동선을 추천받을 수 있어요.
         </p>
       </div>
     </div>
