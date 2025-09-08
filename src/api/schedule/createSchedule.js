5afc1ddf557965a390d48ecc5c0e473d0c87b7c6
Seonghoon Jeong <hooni09061201@gmail.com>
2025-09-08 15:05:24 +0900

    change_schedule_result

--- src/api/schedule/createSchedule.js
+++
@@ -1,6 +0,0 @@
-import http from '../../utils/authAxios';
-
-export async function createSchedule(payload) {
-  const { data } = await http.post('/schedule/create', payload);
-  return data;
-}
--- src/api/schedule/getSchedule.js
+++
@@ -1,7 +0,0 @@
-import http from '../../utils/authAxios';
-
-export async function getSchedule(scheduleId) {
-  if (!scheduleId) throw new Error('scheduleId가 필요합니다.');
-  const { data } = await http.get(`/schedule/details/${scheduleId}`);
-  return data;
-}
