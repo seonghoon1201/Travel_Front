d270b7b7da9fffa9a46d3e088b2e9a5b52cdf0d0
Seonghoon Jeong <hooni09061201@gmail.com>
2025-09-15 22:38:23 +0900

    back_function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AppRoutes from './routes';
+import AppBackHandler from './components/AppBackHandler';
 
 function App() {
   return (
     <div className="bg-background min-h-screen">
+      <AppBackHandler />
       <AppRoutes />
     </div>
   );
